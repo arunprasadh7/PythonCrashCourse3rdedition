4063bb497270c7e33eaed78e219a5f005648d8b8
arunprasadh7 <m.arunprasadh96@gmail.com>
2023-08-24 20:12:25 +0530

    dictionaries1

---
+++ 6 Dictionaries/1 A Simple Dictionary.py
@@ -0,0 +1,7 @@
+#A Simple Dictionary {key:value}
+# dict allows us to connect pieces of information 
+
+alien_0 = {'color': 'green', 'points': 5}
+
+print(alien_0['color'])
+print(alien_0['points'])
