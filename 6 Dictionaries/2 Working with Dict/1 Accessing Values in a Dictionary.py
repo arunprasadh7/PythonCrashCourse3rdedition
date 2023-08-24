1fe639b313bcf2a430ad342d430984eaf1bdbbbf
arunprasadh7 <m.arunprasadh96@gmail.com>
2023-08-24 20:25:21 +0530

    dictionaries

---
+++ 6 Dictionaries/2 Working with Dict/1 Accessing Values in a Dictionary.py
@@ -0,0 +1,8 @@
+#Accessing Values in a Dictionary
+
+alien_0 = {'color': 'green'}
+print(alien_0['color'])
+
+alien_0 = {'color': 'green', 'points': 5}
+new_points = alien_0['points']
+print(f'You just earned {new_points} points!')
