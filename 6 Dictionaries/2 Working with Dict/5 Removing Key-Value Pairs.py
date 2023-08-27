6c02d3d80dba7451eff35be5d60181124626ae08
arunprasadh7 <m.arunprasadh96@gmail.com>
2023-08-27 22:08:19 +0530

    similar dic objects

---
+++ 6 Dictionaries/2 Working with Dict/5 Removing Key-Value Pairs.py
@@ -0,0 +1,12 @@
+# Removing Key-Value Pairs
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0)
+
+del alien_0['points']
+print(alien_0)
+
+alien_0['species'] = 'predator'
+print(alien_0)
+
+del alien_0['species']
+print(alien_0)
---
+++ 6 Dictionaries/2 Working with Dict/6 A Dictionary of Similar Objects.py
@@ -0,0 +1,12 @@
+#A Dictionary of Similar Objects
+
+fav_lang = {
+    'jen':'python',
+    'sarah':'c',
+    'edward':'rust',
+    'phil':'python',
+}
+
+language = fav_lang['sarah'].title()
+
+print(f'Favorite language of Sarah is {language}.')
