69ffe14ce87e391b8101f5174caa8a2e5374eeb1
arunprasadh7 <m.arunprasadh96@gmail.com>
2023-08-29 21:57:43 +0530

    looping dict keys in order

---
+++ 6 Dictionaries/3 Looping Through a Dictionary/3 Looping Through a Dictionary’s Keys in a Particular Order.py
@@ -0,0 +1,11 @@
+#Looping Through a Dictionary’s Keys in a Particular Order
+
+favorite_languages = {
+  'jen': 'python',
+  'sarah': 'c',
+  'edward': 'rust',
+  'phil': 'python',
+}
+
+for name in sorted(favorite_languages.keys()):
+    print(f'Hello {name}. Thanks for taking the poll.')
